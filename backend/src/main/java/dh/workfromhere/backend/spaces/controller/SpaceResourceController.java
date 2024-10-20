fc1f36ddce0eb66f52110d13d40dab88eb2ae16a
David Houston <davidaehouston@gmail.com>
2024-10-20 17:32:45 +0100

    Adding Controller methods

--- backend/src/main/java/dh/workfromhere/backend/spaces/controller/SpaceResourceController.java
+++ backend/src/main/java/dh/workfromhere/backend/spaces/controller/SpaceResourceController.java
@@ -1,7 +1,54 @@
 package dh.workfromhere.backend.spaces.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import dh.workfromhere.backend.spaces.model.SpaceResource;
+import dh.workfromhere.backend.spaces.service.SpaceResourceService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SpaceResourceController {
+
+    private static final String BASE_URL = "/api/v1/spaces/";
+
+    private final SpaceResourceService spaceResourceService;
+
+    public SpaceResourceController(SpaceResourceService spaceResourceService) {
+        this.spaceResourceService = spaceResourceService;
+    }
+
+    @GetMapping(BASE_URL)
+    public ResponseEntity<List<SpaceResource>> getAllSpaces() {
+        return ResponseEntity.ok().body(spaceResourceService.getAllSpaces);
+    }
+
+    @GetMapping(BASE_URL + "{id}")
+    public ResponseEntity<SpaceResource> getSpaceById(@PathVariable String id) {
+        return ResponseEntity.ok().body(spaceResourceService.getSpaceById(id));
+    }
+
+    @PostMapping(BASE_URL)
+    public ResponseEntity<SpaceResource> createSpace(@RequestBody SpaceResource spaceResource) {
+        return new ResponseEntity<SpaceResource>(
+                spaceResourceService.createSpace(spaceResource), HttpStatus.CREATED
+        );
+    }
+
+    @PutMapping(BASE_URL + "{id}")
+    public ResponseEntity<SpaceResource> updateSpace(
+            @PathVariable Integer id,
+            @RequestBody SpaceResource upDatedSpaceResource)
+    {
+        spaceResourceService.updateSpace(id,upDatedSpaceResource);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<SpaceResource> deleteSpace(@PathVariable Integer id) {
+        spaceResourceService.deleteSpaceById(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }

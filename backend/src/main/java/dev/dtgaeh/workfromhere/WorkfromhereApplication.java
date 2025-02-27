83a7d0aba9bfea33199d995d24ec3d09cacd97f0
David Houston <davidaehouston@gmail.com>
2025-02-27 18:21:44 +0000

    Base Commit

---
+++ backend/src/main/java/dev/dtgaeh/workfromhere/WorkfromhereApplication.java
@@ -0,0 +1,13 @@
+package dev.dtgaeh.workfromhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkfromhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkfromhereApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/dtgaeh/workfromhere/WorkfromhereApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dtgaeh.workfromhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkfromhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
